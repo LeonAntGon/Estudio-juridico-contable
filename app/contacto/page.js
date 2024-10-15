75ed558449300322e49807451b681f63208b46ba
unknown <gonzalezleonardoantonio056@gmail.com>
2024-10-15 12:15:43 -0300

    fixing map h sizing

--- app/contacto/page.js
+++ app/contacto/page.js
@@ -152,13 +152,13 @@ export default function Contacto() {
                 <iframe 
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2469.538775768904!2d-65.21186805991198!3d-26.828791268369212!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d005a79bcfd%3A0xe66584a73c8fa173!2sVIEYRA%20%26%20FARIAS%20VIEYRA%20Estudio%20Jur%C3%ADdico%20Contable!5e0!3m2!1sen!2sar!4v1726376030819!5m2!1sen!2sar&hl=es" 
                   width="100%" 
-                  height="100%" 
+                  
                   style={{ border: 0 }} 
                   allowFullScreen="" 
                   loading="lazy" 
                   referrerPolicy="no-referrer-when-downgrade"
                   title="Ubicación de la Oficina"
-                  className='h-[1000px] md:h-auto'
+                  className='h-[1000px] md:h-[%100]'
                 ></iframe>
               </div>
             </CardContent>
