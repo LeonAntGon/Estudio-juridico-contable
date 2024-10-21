0347f047cf7487bff140a5896cfe085544914149
unknown <gonzalezleonardoantonio056@gmail.com>
2024-10-21 12:48:10 -0300

    adding Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import React from 'react';
+import { Analytics } from "@vercel/analytics/react"
 import '@/app/globals.css';
 
 export interface LayoutProps {
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: LayoutProps) {
         
           <Header />
           <main>{children}</main>
+          <Analytics />
           <Footer />
           
       </body>
