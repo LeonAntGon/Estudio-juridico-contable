7288d7d53fa427e3ec5683c23f123c8d23a65879
unknown <gonzalezleonardoantonio056@gmail.com>
2024-10-21 12:53:54 -0300

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import React from 'react';
-import { Analytics } from "@vercel/analytics/react"
 import '@/app/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export interface LayoutProps {
   children: React.ReactNode;
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: LayoutProps) {
         
           <Header />
           <main>{children}</main>
-          <Analytics />
+          <Analytics/>
           <Footer />
           
       </body>
